072ab3f44dfaa19db785f7712e018a60c74c377c
afaqueumer <afaqueumer33@gmail.com>
2025-09-13 21:06:04 +0530

    Patterns - Exercise 01

---
+++ patterns/exercise_01.py
@@ -0,0 +1,19 @@
+# Problem Description: You are given an integer n. Your task is to return a square pattern of size n x n made up of the character '*', 
+# represented as a list of strings.
+
+def generate_square(n):
+    """
+    Function to return a square pattern of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The size of the square.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the square.
+    """
+    # Your code here
+    return [str("*" * n) for i in range(n)]
+
+
+answer = generate_square(5)
+print(answer)
